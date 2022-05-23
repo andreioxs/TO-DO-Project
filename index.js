c47bb66702f659fbc0a2bfbaf689890e36fc3c19
andreas <andreasherweck@gmail.com>
2022-05-23 17:12:01 +0200

    Update 23.05.22

--- index.js
+++ index.js
@@ -20,9 +20,44 @@ function addtaskfunction(e) {
 
 function addtask1(newTask) {
     const newTaskItem = document.createElement('li')
-    newTaskItem.innerText = newTask
+    //console.log(createCheckbox(newTaskItem)).toString()
+    newTaskItem.innerHTML = (createCheckbox(newTaskItem)).outerHTML
+    newTaskItem.innerHTML += (createText(newTask)).outerHTML
+    newTaskItem.innerHTML += (createRemove(newTaskItem,list)).outerHTML
+
     list.appendChild(newTaskItem)
+    //console.log(createCheckbox(newTaskItem))
 }
+function test() {
+    console.log("Hallo")
+}
+
+function createCheckbox(parent) {
+    const checkbox = document.createElement('button')
+    checkbox.innerText = 'createCheckbox'
+    //parent.appendChild(checkbox)
+    return checkbox
+}
+
+function createText(p) {
+    const text = document.createElement('span')
+    text.innerText = p
+    //parent.appendChild(checkbox)
+    return text
+}
+
+function createRemove(r, list) {
+    const removeButton = document.createElement('button')
+    removeButton.innerText = 'remove'
+    removeButton.onclick = "test ()"
+    console.log(list)
+    //removeButton.addEventListener("click" ,test )
+    //removeButton.addEventListener("click" ,()=> list.removeChild(list.lastElementChild) )
+    return removeButton
+}
+
+
+
 
 
 //console.log(newTask)
